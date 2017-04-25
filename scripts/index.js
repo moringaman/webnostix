8631de45983336d42fdcf15b8c1f7a8efb49b2cd
Moringaman <leon@moringamutual.org>
2017-04-25 22:26:56 +0100

    fonts

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,14 @@
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */ /* define */
 
- /* - All our useful JS goes here, awesome! */
-
 ($(document).ready(function () {
     
     "use strict";
     
     var headerModule = {
         // Set initial variables & Call Module functions
+        
+        
         init: function () {
             this.shoNavList = true;
             this.cacheDom();
@@ -25,18 +25,22 @@
             this.$window = $(window);
             this.$element = $('#header');
             this.$sideBar = this.$element.find('#sidebar');
+            this.$btnClose = this.$sideBar.find('#close-button');
             this.$navList = this.$element.find("ul.menu-list");
-            this.$logo = this.$element.find('#logo');
+            this.$logoSection = this.$element.find('.logo');
+            this.$logo = this.$logoSection.find('#logo');
             this.$menuToggle = this.$element.find('#menu-toggle');
+            this.viewPort = this.$window.width();
         },
         // fade in WX logo
-        shoLogo: function (){
+        shoLogo: function () {
             this.$logo.fadeTo(5000, 0.9);
         },
         // Module Event listeners
         bindEvents: function () {
             this.$window.on('resize', this.checkViewPort.bind(this));
             this.$menuToggle.on('click', this.toggleMenu.bind(this));
+            this.$btnClose.on('click', this.toggleMenu.bind(this));
         },
         checkViewPort: function () {
             this.viewPort = this.$window.width();
@@ -45,22 +49,25 @@
         },
         // Toggle the top Navlist dependent on viewport size;
         toggleNav: function (a) {
-            if (a < 667) {
+            if (this.viewPort < 667) {
                 this.$navList.hide(); 
                 this.shoNavList = false; 
-                return this.showNavList;
             } else {
                 this.$navList.show();
                 this.shoNavList = true;
-                return this.showNavList;
             }
         },
        // toggle side bar menu
         toggleMenu: function () {
+            //this.$btnClose.animate()
             this.$sideBar.toggle("slow");
+            if (this.viewPort < 450) {
+                this.$logoSection.fadeToggle(1000);  
+            }
             console.log('toggle ' + this.$sideBar);
             if (this.shoNavList) {
                 this.$navList.fadeToggle(2000);
+    
             }
         }
         
@@ -72,45 +79,3 @@
 }));
 
 
-
- 
-
-/*($(document).ready(function () {
-    
-    "use strict";
-    //DOM Setup
-    
-   headerModule.init(); 
-    console.log("The page is loaded");
-    
-   
-    var a = $('#sidebar'),  logo = $('#logo'),
-     navlist = $("ul.menu-list"),
-     shoNavList = true;
-    
-    $(window).resize(function () {
-    var viewPort = $(window).width();
-        console.log("screen width: " + viewPort);
-        if (viewPort < 667) {
-        navlist.hide(); 
-        return shoNavList = false;     
-    } else {
-        navlist.show();
-        return shoNavList = true;
-    } });
-    
-    
-    
-    a.hide();
-    logo.fadeTo(5000, 0.9);
-    
-    //events
-    $('#menu-toggle').click(function() {
-        a.toggle('slow');
-        if (shoNavList) {
-            navlist.fadeToggle(2000);
-        }
-        
-    });
-
-}));*/
