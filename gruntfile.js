71be695a1f399e65f1ec6fbfb82e862bc33055e8
moringaman <leon@moringamutual.org>
2017-06-24 21:42:28 +0100

    font changes

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "styles/css/index.css": "styles/less/index.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['styles/less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
